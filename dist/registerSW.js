c3cd6c86fb63b3ef2399bd438337473b7b437c68
Jesús Pedro Rodriguez <jesuspedro.rodriguez@unir.net>
2026-02-23 21:46:22 +0100

    Actualizar rutas de archivos en index.html, manifest.webmanifest y registerSW.js para asegurar la correcta referencia en el despliegue

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CTCDailyHours/sw.js', { scope: '/CTCDailyHours/' })})}
